9f4d6b175f3da1e295cca86ce9ee96913854d364
Simon Berlendis <simon.berlendis@sogilis.com>
2025-12-03 13:29:47 +0100

    day 1 part1

--- src/main/kotlin/Day01.kt
+++ src/main/kotlin/Day01.kt
@@ -5,10 +5,13 @@ import java.lang.Math.floorMod
 
 fun main() {
     val testInput = readInput("Day01_test")
-    print(solve(testInput))
+    println("Part 1: ")
+    println(part1(testInput))
+    println("Part 2: ")
+    println(part2(testInput))
 }
 
-fun solve(input: List<String>): Int {
+fun part1(input: List<String>): Int {
     var password = 0
 
     var dial = 50
@@ -16,6 +19,22 @@ fun solve(input: List<String>): Int {
         dial += (if ("R" in instruction) 1 else -1) * instruction.drop(1).toInt()
         dial = floorMod(dial, 100)
         if(dial == 0) password++
+    }
+
+    return password
+}
+
+fun part2(input: List<String>): Int {
+    var password = 0
+
+    var dial = 50
+    for (instruction in input) {
+        val previous = dial
+        dial += (if ("R" in instruction) 1 else -1) * instruction.drop(1).toInt()
+
+        password += (if(dial < 1) (100 - dial) / 100 - (if (previous == 0) 1 else 0) else dial / 100)
+
+        dial = floorMod(dial, 100)
     }
 
     return password
