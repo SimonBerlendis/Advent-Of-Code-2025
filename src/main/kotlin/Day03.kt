eabee96511835028206b7b55a59d15641376646d
Simon Berlendis <simon.berlendis@sogilis.com>
2025-12-04 00:23:03 +0100

    day 3 part 2

--- src/main/kotlin/Day03.kt
+++ src/main/kotlin/Day03.kt
@@ -11,15 +11,24 @@ fun main() {
 }
 
 fun day03_part1(input: List<String>): Int {
-    return input.sumOf { joltage(it) }
-}
-
-fun joltage(bank: String) : Int {
-    val firstDigit = bank.take(bank.length - 1).max()
-    val secondDigit = bank.drop(bank.indexOf(firstDigit) + 1).max()
-    return "$firstDigit$secondDigit".toInt()
+    return input.sumOf { joltage(it, 2) }
 }
 
 fun day03_part2(input: List<String>): Int {
+    return input.sumOf { joltage(it, 12) }
+}
+
+fun joltage(bank: String, nDigits: Int) : Int {
+    if(nDigits == 2){
+        val firstDigit = bank.take(bank.length - 1).max()
+        val secondDigit = bank.drop(bank.indexOf(firstDigit) + 1).max()
+        return "$firstDigit$secondDigit".toInt()
+    } else if (nDigits == 3) {
+        val firstDigit = bank.take(bank.length - 2).max()
+        val secondDigit = bank.substring(bank.indexOf(firstDigit) + 1, bank.length - 1).max()
+        val thirdDigit = bank.drop(bank.indexOf(secondDigit) + 1).max()
+        return "$firstDigit$secondDigit$thirdDigit".toInt()
+    }
+
     return 0
 }
--- src/test/kotlin/Day03Test.kt
+++ src/test/kotlin/Day03Test.kt
@@ -1,31 +1,61 @@
 import org.example.day02_part1
 import org.example.day03_part1
+import org.example.joltage
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
 class Day03Test {
     @Test
     fun day03_part1_12345() {
-        val actual = day03_part1(listOf("12345"))
+        val actual = joltage("12345", 2)
         assertEquals(45, actual)
     }
 
     @Test
     fun day03_part1_987654321111111() {
-        val actual = day03_part1(listOf("987654321111111"))
+        val actual = joltage("987654321111111", 2)
         assertEquals(98, actual)
     }
 
     @Test
     fun day03_part1_234234234234278() {
-        val actual = day03_part1(listOf("234234234234278"))
+        val actual = joltage("234234234234278", 2)
         assertEquals(78, actual)
     }
 
     @Test
     fun day03_part1_818181911112111() {
-        val actual = day03_part1(listOf("818181911112111"))
+        val actual = joltage("818181911112111", 2)
         assertEquals(92, actual)
     }
 
+    @Test
+    fun day03_part2_12345() {
+        val actual = joltage("12345", 3)
+        assertEquals(345, actual)
+    }
+
+    @Test
+    fun day03_part2_987654321111111() {
+        val actual = joltage("987654321111111", 3)
+        assertEquals(987, actual)
+    }
+
+    @Test
+    fun day03_part2_811111111111119() {
+        val actual = joltage("811111111111119", 3)
+        assertEquals(819, actual)
+    }
+
+    @Test
+    fun day03_part2_234234234234278() {
+        val actual = joltage("234234234234278", 3)
+        assertEquals(478, actual)
+    }
+
+    @Test
+    fun day03_part2_818181911112111() {
+        val actual = joltage("818181911112111", 3)
+        assertEquals(921, actual)
+    }
 }
