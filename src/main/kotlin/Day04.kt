93623ffb53a9b465aff8654a095552035e30d81e
Simon Berlendis <simon.berlendis@sogilis.com>
2025-12-06 00:02:35 +0100

    day 4 part 2

--- src/main/kotlin/Day04.kt
+++ src/main/kotlin/Day04.kt
@@ -14,14 +14,27 @@ fun main() {
 
 fun day04_part1(input: List<String>): Int {
     val grid = input.map { line -> line.map { if (it == '.') 0 else 1 } }
-    return countAccessibleRoll(grid)
+    return accessibleRollCount(grid)
 }
 
 fun day04_part2(input: List<String>): Int {
-    return 0
+    val grid = input.map { line -> line.map { if (it == '.') 0 else 1 } }
+    return totalRollCount(grid)
 }
 
-fun countAccessibleRoll(grid: List<List<Int>>): Int {
+fun totalRollCount(grid: List<List<Int>>): Int {
+    val currentGridRollCount = accessibleRollCount(grid)
+    if(currentGridRollCount == 0) return 0
+
+    val newGrid = removeAccessibleRolls(grid)
+    return currentGridRollCount + totalRollCount(newGrid)
+}
+
+fun removeAccessibleRolls(grid: List<List<Int>>) : List<List<Int>> {
+    return grid.mapIndexed { row, line -> line.mapIndexed { col, cell -> if (grid.neighborsRollCount(row, col) < 4) 0 else cell } }
+}
+
+fun accessibleRollCount(grid: List<List<Int>>): Int {
     return grid.flatMapIndexed { row, line -> line.filterIndexed { col, _ -> grid.neighborsRollCount(row, col) < 4 } }.sum()
 }
 
--- src/test/kotlin/Day04Test.kt
+++ src/test/kotlin/Day04Test.kt
@@ -1,5 +1,6 @@
 import org.example.day02_part1
 import org.example.day04_part1
+import org.example.day04_part2
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
@@ -38,6 +39,68 @@ class Day04Test {
             "....",
         )
         assertEquals(4, day04_part1(input))
+    }
+
+    @Test
+    fun day04_part1_survive() {
+        val input = listOf(
+            "....",
+            ".@@.",
+            "@@@@",
+            "@@@@",
+            ".@@.",
+            "....",
+        )
+        assertEquals(0, day04_part1(input))
+    }
+
+    @Test
+    fun day04_part2_circle() {
+        val input = listOf(
+            "....",
+            ".@@@.",
+            ".@.@.",
+            ".@@@.",
+            "....",
+        )
+        assertEquals(8, day04_part2(input))
+    }
+
+    @Test
+    fun day04_part2_border() {
+        val input = listOf(
+            "@@@@",
+            "@...@",
+            "@...@",
+            "@...@",
+            "@@@@",
+        )
+        assertEquals(14, day04_part2(input))
+    }
+
+    @Test
+    fun day04_part2_inside() {
+        val input = listOf(
+            "....",
+            ".@@@.",
+            ".@@@.",
+            ".@@@.",
+            "....",
+        )
+        assertEquals(9, day04_part2(input))
+    }
+
+    @Test
+    fun day04_part2_survive() {
+        val input = listOf(
+            "....",
+            ".@@.",
+            "@@@@",
+            "@@@@",
+            ".@@.",
+            "....",
+        )
+        assertEquals(0, day04_part2(input))
     }
 
 }
