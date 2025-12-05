4d5e51b6606ed2a151a28d86812a032e640120aa
Simon Berlendis <simon.berlendis@sogilis.com>
2025-12-06 00:22:37 +0100

    day 5 start

---
+++ src/main/kotlin/Day05.kt
@@ -0,0 +1,21 @@
+package org.example
+
+import readInput
+import kotlin.math.max
+import kotlin.math.min
+
+fun main() {
+    val testInput = readInput("Day05_test")
+    println("Part 1: ")
+    println(day05_part1(testInput))
+    println("Part 2: ")
+    println(day05_part2(testInput))
+}
+
+fun day05_part1(input: List<String>): Int {
+    return 0
+}
+
+fun day05_part2(input: List<String>): Int {
+    return 0
+}
---
+++ src/test/kotlin/Day05Test.kt
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class Day05Test {
+    @Test
+    fun day05_part1() {
+    }
+
+}
