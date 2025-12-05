10597c4f43eda60009712456aec775c990b2f078
Simon Berlendis <simon.berlendis@sogilis.com>
2025-12-05 03:36:36 +0100

    day 4 start

---
+++ src/main/kotlin/Day04.kt
@@ -0,0 +1,20 @@
+package org.example
+
+import readInput
+
+fun main() {
+    val testInput = readInput("Day04_test")
+    println("Part 1: ")
+    println(day04_part1(testInput))
+    println("Part 2: ")
+    println(day04_part2(testInput))
+}
+
+fun day04_part1(input: List<String>): Int {
+    return 0
+}
+
+fun day04_part2(input: List<String>): Int {
+    return 0
+}
+
