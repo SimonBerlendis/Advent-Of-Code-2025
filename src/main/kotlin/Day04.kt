4af9dad1bcd2e7219c28f99af676023a56d6cdea
Simon Berlendis <simon.berlendis@sogilis.com>
2025-12-05 19:17:02 +0100

    day 4 part 1

--- src/main/kotlin/Day04.kt
+++ src/main/kotlin/Day04.kt
@@ -1,6 +1,8 @@
 package org.example
 
 import readInput
+import kotlin.math.max
+import kotlin.math.min
 
 fun main() {
     val testInput = readInput("Day04_test")
@@ -10,11 +12,37 @@ fun main() {
     println(day04_part2(testInput))
 }
 
+private fun String.convertIntoGridLine(): List<Int> =
+    replace(".", "0").replace("@", "1").split("").filter { it.isNotEmpty() }.map { it.toInt() }
+
 fun day04_part1(input: List<String>): Int {
-    return 0
+    val grid = input.map { line -> line.convertIntoGridLine() }
+    return countAccessibleRoll(grid)
 }
 
 fun day04_part2(input: List<String>): Int {
     return 0
+}
+
+fun countAccessibleRoll(grid: List<List<Int>>): Int {
+    return grid.flatMapIndexed { row, line -> line.filterIndexed { col, _ -> grid.neighborsRollCount(row, col) != 8 } }.sum()
+}
+
+private fun List<List<Int>>.neighborsRollCount(row: Int, col: Int): Int {
+    var result = 0
+
+    val minRow = max(row - 1, 0)
+    val maxRow = min(row + 1, size - 1)
+    for (i in minRow..maxRow) {
+        val line = this[i]
+        val minCol = max(col - 1, 0)
+        val maxCol = min(col + 1, line.size - 1)
+
+        for (j in minCol..maxCol) {
+            if (i == row && j == col) continue
+            result += line[j]
+        }
+    }
+    return result
 }
 
---
+++ src/test/kotlin/Day04Test.kt
@@ -0,0 +1,43 @@
+import org.example.day02_part1
+import org.example.day04_part1
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class Day04Test {
+    @Test
+    fun day04_part1_circle() {
+        val input = listOf(
+            "....",
+            ".@@@.",
+            ".@.@.",
+            ".@@@.",
+            "....",
+        )
+        assertEquals(8, day04_part1(input))
+    }
+
+    @Test
+    fun day04_part1_border() {
+        val input = listOf(
+            "@@@@",
+            "@...@",
+            "@...@",
+            "@...@",
+            "@@@@",
+        )
+        assertEquals(14, day04_part1(input))
+    }
+
+    @Test
+    fun day04_part1_inside() {
+        val input = listOf(
+            "....",
+            ".@@@.",
+            ".@@@.",
+            ".@@@.",
+            "....",
+        )
+        assertEquals(8, day04_part1(input))
+    }
+
+}
