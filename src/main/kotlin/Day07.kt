8575ab6a10da98559d0f8854fcb45756dbdac45f
Simon Berlendis <simon.berlendis@sogilis.com>
2025-12-07 12:24:36 +0100

    day 7 part 1

--- src/main/kotlin/Day07.kt
+++ src/main/kotlin/Day07.kt
@@ -11,8 +11,18 @@ fun main() {
 }
 
 fun day07_part1(input: List<String>): Int {
-    return 1
+    var result = 0
+    var beamPositions = "S".toRegex().findAll(input.first()).map { it.range.first }
+    for (line in input.drop(1)) {
+        val splitterPositions = "\\^".toRegex().findAll(line).map { it.range.first }
+        result += splitterPositions.count { splitter -> beamPositions.any { beam -> beam == splitter } }
+        beamPositions = beamPositions.flatMap { split(it, splitterPositions) }.distinct()
+    }
+    return result
 }
+
+private fun split(beam: Int, splitters: Sequence<Int>): List<Int> =
+    if (splitters.contains(beam)) listOf(beam - 1, beam + 1) else listOf(beam)
 
 fun day07_part2(input: List<String>): Int {
     return 0
--- src/test/kotlin/Day07Test.kt
+++ src/test/kotlin/Day07Test.kt
@@ -10,7 +10,85 @@ class Day07Test {
             "...",
             "...",
         )
+        assertEquals(0, day07_part1(input))
+    }
+
+    @Test
+    fun day07_part1_splitter_untouched() {
+        val input = listOf(
+            ".S.",
+            "^..",
+            "...",
+        )
+        assertEquals(0, day07_part1(input))
+    }
+
+    @Test
+    fun day07_part1_one_splitter() {
+        val input = listOf(
+            ".S.",
+            "...",
+            ".^.",
+            "...",
+        )
         assertEquals(1, day07_part1(input))
     }
 
+    @Test
+    fun day07_part1_two_splitter() {
+        val input = listOf(
+            "..S..",
+            ".....",
+            "..^..",
+            ".....",
+            ".^...",
+            ".....",
+        )
+        assertEquals(2, day07_part1(input))
+    }
+
+    @Test
+    fun day07_part1_splitter_on_same_line() {
+        val input = listOf(
+            "..S...S..",
+            ".........",
+            "..^...^..",
+            ".........",
+        )
+        assertEquals(2, day07_part1(input))
+    }
+
+    @Test
+    fun day07_part1_split_into_same_beam() {
+        val input = listOf(
+            ".S.S.",
+            ".....",
+            ".^.^.",
+            ".....",
+        )
+        assertEquals(2, day07_part1(input))
+    }
+
+    @Test
+    fun day07_part1_use_case() {
+        val input = listOf(
+            ".......S.......",
+            "...............",
+            ".......^.......",
+            "...............",
+            "......^.^......",
+            "...............",
+            ".....^.^.^.....",
+            "...............",
+            "....^.^...^....",
+            "...............",
+            "...^.^...^.^...",
+            "...............",
+            "..^...^.....^..",
+            "...............",
+            ".^.^.^.^.^...^.",
+            "...............",
+        )
+        assertEquals(21, day07_part1(input))
+    }
 }
