48a89da7a6c077dd19d2d4a88e2dba16a3428573
Simon Berlendis <simon.berlendis@sogilis.com>
2025-12-07 22:11:48 +0100

    day07 part 2

--- src/main/kotlin/Day07.kt
+++ src/main/kotlin/Day07.kt
@@ -19,7 +19,7 @@ private fun splitCount(manifold: List<String>, beamPositions: Sequence<Int>): In
     if (manifold.isEmpty()) return 0
     val manifoldLine = manifold.first()
 
-    val splitterPositions = "\\^".toRegex().findAll(manifoldLine).map { it.range.first }
+    val splitterPositions = findSplitters(manifoldLine)
     val splitCount = splitterPositions.count { splitter -> beamPositions.any { beam -> beam == splitter } }
 
     val newBeamPositions = beamPositions.flatMap { split(it, splitterPositions) }.distinct()
@@ -30,6 +30,20 @@ private fun splitCount(manifold: List<String>, beamPositions: Sequence<Int>): In
 private fun split(beam: Int, splitters: Sequence<Int>): List<Int> =
     if (splitters.contains(beam)) listOf(beam - 1, beam + 1) else listOf(beam)
 
+private fun findSplitters(manifoldLine: String): Sequence<Int> =
+    "\\^".toRegex().findAll(manifoldLine).map { it.range.first }
+
 fun day07_part2(input: List<String>): Int {
-    return 0
+    val beamPosition = input.first().indexOf("S")
+    return timelineCount(input, 1, beamPosition)
+}
+
+private fun timelineCount(manifold: List<String>, lineNumber: Int, beamPosition: Int): Int {
+    if (lineNumber == manifold.size) return 1
+
+    return if (manifold[lineNumber][beamPosition] != '^')
+        timelineCount(manifold, lineNumber + 1, beamPosition)
+    else
+        timelineCount(manifold, lineNumber + 1, beamPosition - 1) +
+                timelineCount(manifold, lineNumber + 1, beamPosition + 1)
 }
--- src/test/kotlin/Day07Test.kt
+++ src/test/kotlin/Day07Test.kt
@@ -1,4 +1,5 @@
 import org.example.day07_part1
+import org.example.day07_part2
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
@@ -90,5 +91,85 @@ class Day07Test {
             "...............",
         )
         assertEquals(21, day07_part1(input))
+    }
+
+    @Test
+    fun day07_part2_one_beam() {
+        val input = listOf(
+            ".S.",
+            "...",
+            "...",
+        )
+        assertEquals(1, day07_part2(input))
+    }
+
+    @Test
+    fun day07_part2_splitter_untouched() {
+        val input = listOf(
+            ".S.",
+            "^..",
+            "...",
+        )
+        assertEquals(1, day07_part2(input))
+    }
+
+    @Test
+    fun day07_part2_one_splitter() {
+        val input = listOf(
+            ".S.",
+            "...",
+            ".^.",
+            "...",
+        )
+        assertEquals(2, day07_part2(input))
+    }
+
+    @Test
+    fun day07_part2_two_splitter() {
+        val input = listOf(
+            "..S..",
+            ".....",
+            "..^..",
+            ".....",
+            ".^...",
+            ".....",
+        )
+        assertEquals(3, day07_part2(input))
+    }
+
+    @Test
+    fun day07_part2_timeline_going_same_position() {
+        val input = listOf(
+            ".......S.......",
+            "...............",
+            ".......^.......",
+            "...............",
+            "......^.^......",
+            "...............",
+        )
+        assertEquals(4, day07_part2(input))
+    }
+
+    @Test
+    fun day07_part2_use_case() {
+        val input = listOf(
+            ".......S.......",
+            "...............",
+            ".......^.......",
+            "...............",
+            "......^.^......",
+            "...............",
+            ".....^.^.^.....",
+            "...............",
+            "....^.^...^....",
+            "...............",
+            "...^.^...^.^...",
+            "...............",
+            "..^...^.....^..",
+            "...............",
+            ".^.^.^.^.^...^.",
+            "..............."
+        )
+        assertEquals(40, day07_part2(input))
     }
 }
