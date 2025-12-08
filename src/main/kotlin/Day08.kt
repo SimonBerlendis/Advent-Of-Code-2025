df4d8e84fc0820c4e629248fb28e5dd1eda3b401
Simon Berlendis <simon.berlendis@sogilis.com>
2025-12-09 00:24:21 +0100

    day08 start

---
+++ src/main/kotlin/Day08.kt
@@ -0,0 +1,19 @@
+package org.example
+
+import readInput
+
+fun main() {
+    val testInput = readInput("Day08_test")
+    println("Part 1: ")
+    println(day08_part1(testInput))
+    println("Part 2: ")
+    println(day08_part2(testInput))
+}
+
+fun day08_part1(input: List<String>): Int {
+    return 0
+}
+
+fun day08_part2(input: List<String>): Int {
+    return 0
+}
---
+++ src/test/kotlin/Day08Test.kt
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class Day08Test {
+    @Test
+    fun day08_part1_toto() {
+    }
+
+}
