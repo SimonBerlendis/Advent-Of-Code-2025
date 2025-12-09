2929cb5ff1e3f0f74f9f21c2a9d5758cd01c1dc2
Simon Berlendis <simon.berlendis@sogilis.com>
2025-12-09 10:19:57 +0100

    day08 part 1

--- src/main/kotlin/Day08.kt
+++ src/main/kotlin/Day08.kt
@@ -5,14 +5,37 @@ import readInput
 fun main() {
     val testInput = readInput("Day08_test")
     println("Part 1: ")
-    println(day08_part1(testInput))
+    println(day08_part1(testInput, 1000))
     println("Part 2: ")
     println(day08_part2(testInput))
 }
 
-fun day08_part1(input: List<String>): Int {
-    return 0
+data class Point(val x: Int, val y: Int, val z: Int) {
+    fun distance(otherPoint: Point): Int {
+        return (x - otherPoint.x) * (x - otherPoint.x) +
+                (y - otherPoint.y) * (y - otherPoint.y) +
+                (z - otherPoint.z) * (z - otherPoint.z)
+    }
 }
+
+fun day08_part1(input: List<String>, connectionsMax: Int): Int {
+    val points = getPoints(input)
+
+    val connections = (0..<points.size - 1).flatMap { i -> ((i + 1)..<points.size).map { j -> i to j } }
+        .sortedBy { points[it.first].distance(points[it.second]) }
+        .take(connectionsMax)
+
+    val circuit = mutableListOf<List<Int>>()
+    for (connection in connections) {
+        circuit.add(listOf(connection.first, connection.second))
+    }
+
+    return circuit.map { it.size }.sorted().take(3).reduce { acc, i -> acc * i }
+}
+
+private fun getPoints(input: List<String>): List<Point> =
+    input.map { line -> line.split(",").map { it.toInt() } }
+        .map { Point(it[0], it[1], it[2]) }
 
 fun day08_part2(input: List<String>): Int {
     return 0
--- src/test/kotlin/Day08Test.kt
+++ src/test/kotlin/Day08Test.kt
@@ -1,9 +1,33 @@
-import org.junit.jupiter.api.Assertions.*
+import org.example.day08_part1
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class Day08Test {
     @Test
     fun day08_part1_toto() {
+        val input = listOf(
+            "162,817,812",
+            "57,618,57",
+            "906,360,560",
+            "592,479,940",
+            "352,342,300",
+            "466,668,158",
+            "542,29,236",
+            "431,825,988",
+            "739,650,466",
+            "52,470,668",
+            "216,146,977",
+            "819,987,18",
+            "117,168,530",
+            "805,96,715",
+            "346,949,466",
+            "970,615,88",
+            "941,993,340",
+            "862,61,35",
+            "984,92,344",
+            "425,690,689",
+        )
+        assertEquals(40, day08_part1(input, 10))
     }
 
 }
